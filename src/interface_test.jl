0839b2417b2e4ace23108ad60d03c4e654cc57f3
Oliver Schulz <oschulz@mpp.mpg.de>
2021-11-03 04:51:08 +0100

    Revert "Tighten logfuncdensity test in test_density_interface"
    This reverts commit 1434c9cc5cd5c59ba7c733465a9781437a5aafb8.

--- src/interface_test.jl
+++ src/interface_test.jl
@@ -9,7 +9,8 @@ Tests that [`logdensityof(d, x)`](@ref) equals `ref_logd_at_x` and
 that the behavior of [`logdensityof(d)`](@ref),
 [`densityof(d, x)`](@ref) and [`densityof(d)`](@ref) is consistent.
 
-Also tests if `logfuncdensity(logdensityof(d)) == d`.
+Also tests if `logfuncdensity(logdensityof(d))` returns
+a density equivalent to `d` in respect to the functions above.
 """
 function test_density_interface(d, x, ref_logd_at_x)
     @testset "test_density_interface: $d with input $x" begin
@@ -22,6 +23,12 @@ function test_density_interface(d, x, ref_logd_at_x)
         @test densityof(d,x) == ref_d_at_x
         @test densityof(d)(x) == ref_d_at_x
 
-        @test logfuncdensity(logdensityof(d)) == d
+        d2 = logfuncdensity(log_f)
+        @test hasdensity(d2) == true
+        @test logdensityof(d2, x) == ref_logd_at_x
+        log_f2 = logdensityof(d2)
+        @test log_f2(x) == ref_logd_at_x
+        @test densityof(d2,x) == ref_d_at_x
+        @test densityof(d2)(x) == ref_d_at_x
     end
 end
